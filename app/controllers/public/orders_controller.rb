56c10940875611d693fa9f13912cd89269de3654
xion0207 <98865688+xion0207@users.noreply.github.com>
2022-05-04 13:59:23 +0000

    atohalogingenteinomi

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -4,22 +4,22 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
-    if @order = Order.new
-      @cart_items = CartItem.where(customer_id: current_customer.id )
-      @total = @cart_items.inject(0) { |sum, item| sum + item.subtotal }
-      @order = Order.new(order_params)
-      if params[:order][:select_address] == "0"
-         @order.postal_code = current_customer.postal_code
-         @order.address = current_customer.address
-         @order.name = current_customer.last_name + current_customer.first_name
-      end
-      if params[:order][:select_address] == "1"
-         @address = Address.find(params[:order][:address_id])
-         @order.postal_code = @address.postal_code
-         @order.address = @address.address
-         @order.name = @address.name
-      end
-    else
+    @cart_items = CartItem.where(customer_id: current_customer.id )
+    @total = @cart_items.inject(0) { |sum, item| sum + item.subtotal }
+    @postage = 800
+    @order = Order.new(order_params)
+    if params[:order][:select_address] == "0"
+       @order.postal_code = current_customer.postal_code
+       @order.address = current_customer.address
+       @order.name = current_customer.last_name + current_customer.first_name
+    end
+    if params[:order][:select_address] == "1"
+       @address = Address.find(params[:order][:address_id])
+       @order.postal_code = @address.postal_code
+       @order.address = @address.address
+       @order.name = @address.name
+    end
+    if params[:order][:select_address] == nil
       redirect_to new_order_path
     end
   end
@@ -28,15 +28,33 @@ class Public::OrdersController < ApplicationController
   end
 
   def create
+    @order = Order.new(order_params)
+    @order.save
+    @cart_items = CartItem.where(customer_id: current_customer.id )
+    @cart_items.each do |cart_item|
+      @order_detail = OrderDetail.create({ id: Order.new(order_params), item_id: cart_item.item_id, order_id: @order.id , amount: cart_item.amount, making_status: 0 , tax_included_price: cart_item.item.add_tax_price })
+    end
+    @cart_item = current_customer.cart_items.destroy_all
+    redirect_to orders_complete_path
   end
 
   def index
+    @customer = current_customer
+    @orders = @customer.orders
   end
 
   def show
+    @order = Order.find(params[:id])
+    @order_details = OrderDetail.where(order_id: @order.id)
   end
 
+  private
+
   def order_params
-    params.require(:order).permit(:item_id, :customer_id, :subtotal, :payment_method, :status, :postal_code, :address, :name, :postage)
+    params.require(:order).permit(:customer_id, :subtotal, :payment_method, :status, :postal_code, :address, :name, :postage)
+  end
+
+  def order_detail_params
+      params.require(:order_detail).permit(:item_id, :order_id, :amount, :making_status, :tax_included_price)
   end
 end
